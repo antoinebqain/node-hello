67c03cfefbfe1ef65bfd664cc4f7a9aab1d5bb70
Tony <tony@mobiworld.org>
2021-05-10 14:02:36 +0300

    adding changes for 1.16.11

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+const { exception } = require('console');
 const http = require('http');
 const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = `App Version ${process.env.AppVersion}`
+  throw exception('test')
   res.end(msg);
 });
 
