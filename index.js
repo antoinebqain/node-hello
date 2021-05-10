24f38115b8fd0146913ec9a4b1cb345ca9d5c40e
Tony <tony@mobiworld.org>
2021-05-10 13:54:44 +0300

    adding changes for 1.16.8

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = 80;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node! v50\n'
+  const msg = `App Version ${process.env.AppVersion}`
   res.end(msg);
 });
 
